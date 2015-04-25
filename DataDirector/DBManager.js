1d046230f7a772664219fa1e74634c92cc1469a8
Jack <sokin1@hotmail.com>
2015-04-25 18:42:12 -0400

    Worked on DB

--- DataDirector/DBManager.js
+++ DataDirector/DBManager.js
@@ -2,60 +2,61 @@
 var User = require( "../App-Server/User" );
 
 function DBManager() {
-    // var db = mysql.createConnection({
-    //     host : 'localhost',
-    //     port : '3306',
-    //     user : 'dbadmin',
-    //     password : 'W1Ornr123',
-    //     database : 'FamilyShare'
-    // });
+    var db = mysql.createConnection({
+        host : 'localhost',
+        port : '3306',
+        user : 'dbadmin',
+        password : 'W1Ornr123',
+        database : 'FamilyShare'
+    });
 
-    // db.connect( function( err ) {
-    //     if( err ) throw err;
-    // });
+    db.connect( function( err ) {
+        if( err ) throw err;
+    };
 
-    this.request = function() {
-        
+    this.request = function( reqBody, callback ) {
+        if( reqBody.command == "REQ_GETUSERINFO" || reqBody.command == "REQ_LOGIN" ) {
+            getUserInfo( reqBody.subject, function( err, rows ) {
+                if( err ) callback( err, null );
+                else callback( null, new User( rows[''], rows[''], rows[''], rows[''], rows[''] ) );
+            });
+        } else if( reqBody.command == "REQ_SIGNUP" ) {
+            registerUser( reqBody, subject, function( err, rows ) {
+                if( err ) callback( err, null );
+                else callback( null, new User( rows[''], rows[''], rows[''], rows[''], rows[''] ) );
+            });
+        }
     }
 
-    this.getUserInfo = function( userInfo, callback ) {
+    var getUserInfo = function( userInfo, callback ) {
         var key1 = userInfo['ID'];
+        // password should be encrypted before coming to here.
         var key2 = userInfo['password'];
 
-        // db.query( 'SELECT * FROM User WHERE userName = ? AND password = ?', [key1, key2], function( err, rows, fields ) {
-        //     if( err ) callback( err, new Error( 'DBERROR-Cannot Run Query' ) );
-        //     else {
-        //         if( rows.length == 1 ) {
-        //             callback( null, new User( key1, false ) );
-        //         } else if( rows.length == 0 ) {
-        //             callback( null, new Error( 'No Such User' ) );
-        //         } else {
-        //             callback( null, new Error( 'DBERROR-Too Many Results' ) );
-        //         }
-        //     }
-        // });
+        // What about fields?
+        db.query( 'SELECT * FROM User WHERE userName = ? AND password = ?', [key1, key2], function( err, rows, fields ) {
+            if( err ) callback( new Error( 'DBERROR-Cannot Run Query' ), null );
+            if else ( rows.length > 1 ) callback( new Error( 'DBERROR-Database crashed' ), null );
+            if else ( rows.length == 0 ) callback( new Error( 'USER-User is not registered' ), null );
+            else callback( null, rows );
+        });
     }
 
-    this.setUserInfo = function( newUser, callback ) {
+    var registerUser = function( newUser, callback ) {
+        // TODO : Modify query for insert
         var post = { userName : newUser['ID'], password : newUser['password'] };
         var key = userId;
 
-        // db.query( 'SELECT * FROM User WHERE userName = ?', [key], function( err, rows, fields ) {
-        //     if( err ) callback( err, null );
-        //     if( rows.length >= 1 ) {
-        //         callback( new Error( "Existing User" ) );
-        //     } else {
-        //         db.query( 'INSERT INTO User SET ?', post, function( err, result ) {
-        //             if( err ){
-        //                 callback( err );
-        //             } else {
-        //                 newUser.setConCondition( "SIGNUP_REGISTERED" );
-        //                 callback( newUser );
-        //             }
-        //         });
-        //     }
-        // });
-        callback( newUser );
+        db.query( 'SELECT * FROM User WHERE userName = ?', [key], function( err, rows, fields ) {
+            if( err ) callback( new Error( "DBError-Cannot Run Query" ), null );
+            if( rows.length >= 1 ) callback( new Error( "USER-User Already Registered" ), null );
+            } else {
+                db.query( 'INSERT INTO User SET ?', post, function( err, result ) {
+                    if( err ) callback( new Error( 'DBError-Cannot Run Query' ), null );
+                    else callback( null, newUser );
+                });
+            }
+        });
     }
 
     this.getGroupInfo = function( user, callback ) {
